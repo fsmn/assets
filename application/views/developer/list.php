eaa60c670d8029b2b8426ccb7a247e48874fa72b
Chris Dart <chrisdart@cerebratorium.com>
2014-08-04 12:08:24 -0500

    list management

--- application/views/developer/list.php
+++ application/views/developer/list.php
@@ -3,7 +3,7 @@
 echo $links;
 
 foreach($developers as $developer){
-	echo "<fieldset><legend>$developer->developer</legend>";
+	echo "<fieldset class='column-3'><legend>$developer->developer</legend>";
 	echo "<p><span class='button edit developer_edit' id='de_$developer->kDeveloper'>Edit</span>&nbsp;";
 	echo "<a href='". site_url("developer/view/$developer->kDeveloper") ."' class='button'>List Assets</a></p></fieldset>";
 }
